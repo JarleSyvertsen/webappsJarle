bea028db7a979df0df3b4342738484dd616341d8
Jarle <jarlesyv@hiof.no>
2023-08-29 11:41:49 +0200

    Laster opp for oppgavesvar.

---
+++ addnumbers/addnumbers.js
@@ -0,0 +1,30 @@
+document.getElementById("sort").addEventListener("click", sort);
+document.getElementById("addnumber").addEventListener("click", addnumber);
+
+var pushIndex = 0;
+
+function getValues() {
+    const valueStore = document.querySelectorAll('.container')[0];
+    return Array.from(valueStore.children);
+}
+function sort() {
+    valueBoxes = getValues();
+    const sortedValues = [];
+
+    valueBoxes.forEach((input) => {
+        sortedValues.push(input.value);
+    })
+
+    sortedValues.sort(function(a, b) {return a - b;});
+
+    valueBoxes.forEach((input) => {
+        input.value = sortedValues.shift();
+    })
+}
+
+function addnumber() {
+    valueBoxes = getValues();
+    valueBoxes.forEach((input) => {
+        input.value = Math.floor(Math.random() * 100);
+    })
+}
