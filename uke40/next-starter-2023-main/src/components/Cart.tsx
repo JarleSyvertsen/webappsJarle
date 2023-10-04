ccdda2d9a3fe39b7a64879e8836b4ecb33e7b364
Jarle <jarlesyv@hiof.no>
2023-10-04 22:28:01 +0200

    Cart toggle

--- uke40/next-starter-2023-main/src/components/Cart.tsx
+++ uke40/next-starter-2023-main/src/components/Cart.tsx
@@ -1,8 +1,10 @@
 import CartRow from "@/components/CartRow";
 import {cartButtons, cartMap, productMap} from "@/types/types";
+import {useState} from "react";
 
 export default function Cart(props: productMap & cartMap & cartButtons) {
     const {products, cart, deleteButton, increaseButton, decreaseButton} = props;
+    const [toggled, setToggled] = useState(false);
     const cartList = Array.from(cart.values());
 
     function productNameFromId(id: String) {
@@ -11,11 +13,14 @@ export default function Cart(props: productMap & cartMap & cartButtons) {
     }
 
     return (
-        <section id="cart" className="cart z-10 absolute border-2 bg-white w-1/3">
+        <>
+        <button onClick={() => setToggled((prev) => !prev)}>Open Cart</button>
+        <section id="cart" className={toggled ? "cart z-10 mx-5 min-h-full transition-transform absolute border-2 bg-white w-1/3": "cart hidden transition-transform"}>
             {cartList.map((cartItem) =>
                 <CartRow deleteButton={deleteButton} increaseButton={increaseButton} decreaseButton={decreaseButton} key={cartItem.itemId} itemId={cartItem.itemId} itemName={productNameFromId(cartItem.itemId)} count={cartItem.count}></CartRow>
             )}
         </section>
+        </>
     )
 
 }
