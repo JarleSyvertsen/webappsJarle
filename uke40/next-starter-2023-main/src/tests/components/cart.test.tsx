d0c1c00b11f02c69fe7588780e3d99a20200aa8d
Jarle <jarlesyv@hiof.no>
2023-10-04 19:05:40 +0200

    Lagt til resten av cart modul sine tester.

--- uke40/next-starter-2023-main/src/tests/components/cart.test.tsx
+++ uke40/next-starter-2023-main/src/tests/components/cart.test.tsx
@@ -4,7 +4,6 @@ import { createProducts } from "@/features/products/generateProducts";
 import useCart from "@/hooks/useCart";
 import {act} from "react-dom/test-utils";
 import userEvent from "@testing-library/user-event";
-import {beforeAll} from "vitest";
 import { cartData, productInfo} from "@/types/types";
 import {MouseEventHandler} from "react";
 
@@ -61,5 +60,49 @@ describe("Cart functions should behave properly", () => {
         const itemCount = firstElement?.getElementsByClassName('itemCount')[0].innerHTML;
 
         expect(itemCount).toBe("2x");
+    })
+    test("Decrement button button should not reduce amount under zero.", async () => {
+        const {rerender, products, result } = await setupCart()
+
+        const firstElement = document.querySelector(".cartElement");
+        const decreaseButton = firstElement?.getElementsByClassName('btnDecrease')[0] as Element;
+        await act(async () => {
+            await userEvent.click(decreaseButton)
+        })
+
+        rerender(buildCart(products, result))
+        const itemCount = firstElement?.getElementsByClassName('itemCount')[0].innerHTML;
+
+        expect(itemCount).toBe("1x");
+    })
+    test("Decreasing element by one after two increments should leave the value at two.", async () => {
+        const {rerender, products, result } = await setupCart()
+
+        const firstElement = document.querySelector(".cartElement");
+        const incrementButton = firstElement?.getElementsByClassName('btnIncrement')[0] as Element;
+        const decreaseButton = firstElement?.getElementsByClassName('btnDecrease')[0] as Element;
+        await act(async () => {
+            await userEvent.click(incrementButton)
+            await userEvent.click(incrementButton)
+            await userEvent.click(decreaseButton)
+        })
+
+        rerender(buildCart(products, result))
+        const itemCount = firstElement?.getElementsByClassName('itemCount')[0].innerHTML;
+
+        expect(itemCount).toBe("2x");
+    })
+    test("Deleting an element should remove it from the list.", async () => {
+        const {rerender, products, result } = await setupCart()
+        const cartRows = document.querySelector("#cart")
+
+        const firstElement = cartRows?.querySelector(".cartElement");
+        const deleteButton = firstElement?.getElementsByClassName('btnDelete')[0] as Element;
+        await act(async () => {
+            await userEvent.click(deleteButton)
+        })
+
+        rerender(buildCart(products, result))
+        expect(cartRows?.children.length).toBe(0)
     })
 })
